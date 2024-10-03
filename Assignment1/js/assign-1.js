ec49f2d3b4276db4c39a4c363e989d6ce7462dc6
aaliyah033 <79179237+aaliyah033@users.noreply.github.com>
2024-10-02 21:13:40 -0400

    step 2: added an array of words to be stored in the columns

--- Assignment1/js/assign-1.js
+++ Assignment1/js/assign-1.js
@@ -2,5 +2,11 @@
 const storymaker = document.querySelector('.story');
 const game = document.querySelector('.game-story');
 
-
+//STEP 2: array containing the column/row with the words
+//added words from the book 
+const column1 = ['the turket', 'Mom', 'Dad', 'The dog', 'My teacher', 'The elephant', 'The cat'];
+const column2 = ['sat on', 'ate', 'danced with', 'saw', 'doesn\t', 'kissed'];
+const column3 = ['a funny', 'a scary', 'a goofy', 'a slimy', 'a barking', 'a fat'];
+const column4 = ['goat', 'monkey', 'fish', 'cow', 'frog', 'bug', 'worm'];
+const column5 = ['on the moon', 'on the chair', 'in my spaghetti', 'in my soup', 'on the grass', 'in my shoes'];
 
