c407efdd05185bd65c422c4ae1ca1bd5809d7b09
aaliyah033 <79179237+aaliyah033@users.noreply.github.com>
2024-10-16 10:16:50 -0400

    added instructions for the lab

---
+++ lab-2/js/page-color-changer.js
@@ -0,0 +1,16 @@
+//Instructions:
+
+// Use the below instructions as a framework for developing your interactive interface:
+
+//     Analyze the provided HTML, CSS, and JavaScript files (see the bottom of this page).
+//     You may not change any part of the HTML or the CSS files for this lab - this is an integral part of the challenge.
+//     Create a local and a remote code repository for the 
+//     Open the file called page-color-changer.js.
+//     Declare and initialize some constants for the input slider elements, the page body and any other HTML element you think you need to manipulate.
+//     Build one or more event handlers to call a function when the user changes the value of one of the range sliders (hint - look up the code for the weather page where we built a slider for the wind speed).
+//     Build one or more functions that change the background-color property value of the page body (or HTML element), based on the values of the three range sliders (hint - we changed the background-color of the page in a prior lesson).
+//     Make sure that your event handler(s) call the above function(s) you created.
+//     Test the application in a browser to track down any errors - use the console.
+//     Comment all major code blocks or pieces of functionality.
+//     Do your best and have fun - resist the temptation to simply look for a complete solution online or from generative AI - this is your opportunity to collaborate with your peers and to try to apply some of the things that you have learned.
+
