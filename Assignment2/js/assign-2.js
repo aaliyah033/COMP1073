0a52b337b1f0bf788032265de59863a17d8494d9
aaliyah033 <79179237+aaliyah033@users.noreply.github.com>
2024-11-07 01:02:59 -0500

    re did to mkae the code work

--- Assignment2/js/assign-2.js
+++ Assignment2/js/assign-2.js
@@ -167,45 +167,73 @@ class Smoothie {
 document.getElementById("order").addEventListener("click", function(){
     //STEP SEVEN: as well as taregting the clicked values
     //NOTE: using the values from the radio and checkbox
-    var fruit = document.querySelector('input[name="fruit"]:checked');
-    var booster = document.querySelector('input[name="booster"]:checked');
-    var ice = document.querySelector('input[name="ice"]:checked');
-    var topping = document.querySelector('input[name="topping"]:checked');
-    var liquid = document.querySelector('input[name="liquid"]:checked');
-    var size = document.querySelector('input[name="size"]:checked');
+    var fruit = document.getElementsByName("fruit");
+    var booster = document.getElementsByName("booster");
+    var ice = document.getElementsByName("ice");
+    var topping = document.getElementsByName("topping");
+    var liquid = document.getElementsByName("liquid");
+    var size = document.getElementsByName("size");
 
-    //adding an if statement that will check if nothing and will set value --- this to fix and get the output
-    if(fruit !== null){
-        fruit = fruit.value;
+    //STEP SEVEN B: I now tried intializing to anther empty string, then a loop that loops through each checked box and if checked then stores to the empty string follwed with comma
+    //NOTES: this is for access the paragraph displaying the information
+    var fruitInfo = "";
+    for (var i = 0; i < fruit.length; i++) {
+        if (fruit[i].checked) {
+            fruitInfo += fruit[i].value + ", "; 
         }
-        else{
-            fruit = '';
-            }
-    if(booster !== null){
-        booster = booster.value;
+    }
+    console.log(fruitInfo); //to check if working
+    
+
+    var boosterInfo = "";
+    for (var i = 0; i < booster.length; i++) {
+        if (booster[i].checked) {
+            boosterInfo += booster[i].value + ", "; 
         }
-        else{booster = ''; }
-    if(ice !== null){
-        ice = ice.value;
+    }
+    console.log(boosterInfo);//to check if working
+    
+
+    var toppingInfo = "";
+    for (var i = 0; i < topping.length; i++) {
+        if (topping[i].checked) {
+            toppingInfo += topping[i].value + ", "; 
         }
-        else{ice = ''; }
-    if(topping !== null){
-        topping = topping.value;
+    }
+    console.log(toppingInfo);//to check if working
+
+
+    var liquidInfo = "";
+    for (var i = 0; i < liquid.length; i++) {
+        if (liquid[i].checked) {
+            liquidInfo += liquid[i].value+ ", ";
         }
-        else{topping = ''; }
-    if(liquid !== null){
-        liquid = liquid.value;
+    }
+    console.log(liquidInfo);//to check if working
+
+    var sizeInfo = "";
+    for (var i = 0; i < size.length; i++) {
+        if (size[i].checked) {
+            sizeInfo += size[i].value + ", ";
+          
         }
-        else{liquid = ''; }
-    if(size !== null){
-        size = size.value;
+    }
+    console.log(sizeInfo);//to check if working
+
+    var iceInfo = "";
+    for (var i = 0; i < ice.length; i++) {
+        if (ice[i].checked) {
+            iceInfo += ice[i].value+ ", ";
         }
-        else{size = ''; }           
+    }
+    console.log(iceInfo);//to check if working
+      
     
     // STEP EIGHT: after being accesed from the objects shows order information
     //from class Smoothie Instatiate  based on the  constructor - reference from coffee.js (step2)
-    let smoothie = new Smoothie(size,fruit,liquid,topping,booster,ice);
+    let smoothie = new Smoothie(sizeInfo,fruitInfo,liquidInfo,toppingInfo,boosterInfo,iceInfo);
     smoothie.getOrderSmoothie();
+    console.log(smoothie);//to check if working
   
 
 });
