a2708100634a637caf31e901ca3a68f1c78f589d
aaliyah033 <79179237+aaliyah033@users.noreply.github.com>
2024-11-06 21:34:40 -0500

    Added STEP 4: if statement and switch case for user to choose what they wnat

--- Assignment2/js/assign-2.js
+++ Assignment2/js/assign-2.js
@@ -27,6 +27,134 @@ class Smoothie {
     var smoothieInformation ="This is your order details:";
 
     //STEP FOUR: an if statement that will checks if user has pressed on the buttons, and since i have some ino for user to choose i kept switch statement so it chechks which is chosen and will add statement to the string variable 
-   }
+    
+    //Targeting the size  
+    if(size){
+        switch(size){
+            case 'small':
+                smoothieInformation += `Your smoothie size is ${size} `;
+                break;
+            case 'medium':
+                smoothieInformation += `Your smoothie size is ${size} `;
+                break;
+            case 'large':
+                smoothieInformation += `Your smoothie size is ${size} `;
+                break;
+            default:
+                smoothieInformation += `Your smoothie size is ${size} `;
+        }
+    }
+    //Targeting the fruit
+    if(fruit){
+        switch(fruit){
+            case 'banana':
+                smoothieInformation += `Your smoothie fruit is ${fruit} `;
+                break;
+            case 'strawberry':
+                smoothieInformation += `Your smoothie fruit is ${fruit} `;
+                break;
+            case 'mango':
+                smoothieInformation += `Your smoothie fruit is ${fruit} `;
+                break;
+            case 'blueberry':
+                smoothieInformation += `Your smoothie fruit is ${fruit} `;
+                break;
+            case 'green':
+                smoothieInformation += `Your smoothie fruit is ${fruit} `;
+                break;
+            case 'chocolate':
+                smoothieInformation += `Your smoothie fruit is ${fruit} `;
+                break;
+
+            default:
+                smoothieInformation += `Your smoothie fruit is ${fruit} `;         
+    }
+    //Targeting the topping
+    if(topping){
+        switch(topping){
+            case 'chai-seeds':
+                smoothieInformation += `Your smoothie protein is ${topping} `;
+                break;
+            case 'tapioca':
+                smoothieInformation += `Your smoothie protein is ${topping} `;
+                break;
+            case 'mango-chunks':
+                smoothieInformation += `Your smoothie protein is ${topping} `;
+                break;
+            case 'grass-jelly':
+                smoothieInformation += `Your smoothie protein is ${topping} `;
+                break;
+            default:
+                smoothieInformation += `Your smoothie protein is ${topping} `;      
+        }
+
+    }
+    //Targeting the liquid
+    if(liquid){
+        switch(liquid){
+            case 'lactose-free milk':
+                smoothieInformation += `Your smoothie liquid is ${liquid} `;
+                break;
+            case 'full-milk':
+                smoothieInformation += `Your smoothie liquid is ${liquid} `;
+                break;
+            case 'almond-milk':
+                smoothieInformation += `Your smoothie liquid is ${liquid} `;
+                break;
+            case 'oat-milk':
+                smoothieInformation += `Your smoothie liquid is ${liquid} `;
+                break;
+            case 'soy-milk':
+                smoothieInformation += `Your smoothie liquid is ${liquid} `;
+                break;
+            case 'coconut-milk':
+                smoothieInformation += `Your smoothie liquid is ${liquid} `;
+                break;
+            default:
+                smoothieInformation += `Your smoothie liquid is ${liquid} `;
+
+        }
+    }
+    //Targeting the booster
+    if(booster){
+        switch(booster){
+            case 'Combo':
+                smoothieInformation += `Your smoothie booster is ${booster} `;
+                break;
+            case 'Energy':
+                smoothieInformation += `Your smoothie booster is ${booster} `;
+                break;
+            case 'Vitamin':
+                smoothieInformation += `Your smoothie booster is ${booster} `;
+                break;
+            case 'Power':
+                smoothieInformation += `Your smoothie booster is ${booster} `;
+                break;
+            case 'Density':
+                smoothieInformation += `Your smoothie booster is ${booster} `;
+                break;
+            default:
+                smoothieInformation += `Your smoothie booster is ${booster} `;
+        }
+    }
+    //Targeting the Ice
+    if(ice){
+        switch(ice){
+            case 'no"':
+                smoothieInformation += `Your smoothie has ice `;
+                break;
+            case 'regular':
+                smoothieInformation += `Your smoothie does not have ice `;
+                break;
+            case 'extra':
+                smoothieInformation += `Your smoothie does not have ice `;
+                break;
+            default:
+                smoothieInformation += `Your smoothie has ice `;
+        }
+    }
+    // STEP FIVE: to make the paraghraph visible from the user chose
+}
+}
 }
 
