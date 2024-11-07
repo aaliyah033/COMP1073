ffba4eb5e4a54e5d423b5b7c1f21a441cc13119a
aaliyah033 <79179237+aaliyah033@users.noreply.github.com>
2024-11-06 20:52:38 -0500

    STEP 2: added class objects and constructors

--- Assignment2/js/assign-2.js
+++ Assignment2/js/assign-2.js
@@ -1,3 +1,23 @@
 //STEP ONE: decalring and initialize the columns
 let right = document.querySelector('.right-side');
 let left = document.querySelector('.left-side');
+
+//STEP TWO: creating class, objects, and constructors
+//NOTE: reerence from class code, step 1 (coffee.js)
+class Smoothie {
+    size;
+    fruit;
+    liquid
+    topping;
+    booster;
+    ice;
+
+    constructor(size,fruit,liquid,topping,booster,ice){
+        this.size = size;
+        this.fruit = fruit;
+        this.liquid = liquid;
+        this.topping = topping;
+        this.booster = booster;
+        this.ice = ice;
+    }
+}
