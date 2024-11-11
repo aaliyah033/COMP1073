3b44255451d7bf2a2f243672b121d9bbd7b5fa0e
aaliyah033 <79179237+aaliyah033@users.noreply.github.com>
2024-11-11 18:36:57 -0500

    added JS: class, properties and constructors

---
+++ Lab3/js/toyCar.js
@@ -0,0 +1,23 @@
+const target1 = document.querySelector(".car-Center");
+
+// STEP ONE: Add car constructors, object properties 
+// notes: --- try finding 10-15 --- ref: coffee.js
+class ToyCars {
+    constructor (name, color, brand, manufacturer, scale, material, modelNumber, tire, country, year, sku, seats, trunk, doors){
+    this.name = name;
+      this.color = color;
+      this.brand = brand;
+      this.manufacturer = manufacturer;
+      this.scale = scale;
+      this.material = material;
+      this.modelNumber = modelNumber;
+      this.tire = tire;
+      this.country = country;
+      this.year = year;
+      this.sku = sku;
+      this.seats = seats;
+      this.trunk = trunk;
+      this.doors = doors;
+      }
+}
+
