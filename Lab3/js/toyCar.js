05ed33ae1089a43c4eaf50538d5452e13017b7ff
aaliyah033 <79179237+aaliyah033@users.noreply.github.com>
2024-11-12 13:19:46 -0500

    step three: for result

--- Lab3/js/toyCar.js
+++ Lab3/js/toyCar.js
@@ -23,7 +23,94 @@ class ToyCars {
 
 //STEP TWO: default car inventory
 var inventoryCars=[
-  new Car("Ford Bronco","yellow","Ford","Canada","1:24","travel car","plastic","31530OR","4","2021","31530OR-MAI-ORANGE","4","Yes","2"),
-  new Car("super Beetle Convertable","blue","Volkswagen","1:18","classic car","copper","680334688657","4","Germny","1957","JLSP107A-JL-BLUE","2","NO","2"),
-  new Car("Audi RS","White","Audi","USA","1:18","sports car","plastic","680334691930","4","USA","2020","GT302-GT-WHITE","4","Yes","4")
+  new ToyCars("Ford Bronco","yellow","Ford","Canada","1:24","travel car","plastic","31530OR","4","2021","31530OR-MAI-ORANGE","4","Yes","2"),
+  new ToyCars("super Beetle Convertable","blue","Volkswagen","1:18","classic car","copper","680334688657","4","Germny","1957","JLSP107A-JL-BLUE","2","NO","2"),
+  new ToyCars("Audi RS","White","Audi","USA","1:18","sports car","plastic","680334691930","4","USA","2020","GT302-GT-WHITE","4","Yes","4")
 ];
+
+//STEP THREE: Add a function to display the inventory details
+function displayInventory(Car){
+  const outputInventory = document.getElementById('result-view');
+
+  //STEP 3b: will create so to append the details - basically for the heading on the details part
+  //notes: refrenece to lesson-9
+  const heading = document.createElement('h3');
+  heading.textContent = "Toy Car Inventory Details";
+  outputInventory.appendChild(heading);
+  
+  //STEP 3c: adding the name for detail display
+   if (Car.name){
+    const name = document.createElement('p');
+    name.textContent = `Name: ${Car.name}`;
+    outputInventory.appendChild(name);
+   }
+   if (Car.color){
+    const color = document.createElement('p');
+    color.textContent = `Color: ${Car.color}`;
+    outputInventory.appendChild(color);
+   }
+   if (Car.brand){
+    const brand = document.createElement('p');
+    brand.textContent = `Brand: ${Car.brand}`;
+    outputInventory.appendChild(brand);
+
+   }
+   if (Car.manufacturer){
+    const manufacturer = document.createElement('p');
+    manufacturer.textContent = `Manufacturer: ${Car.manufacturer}`;
+    outputInventory.appendChild(manufacturer);
+   }
+   if (Car.scale){
+    const scale = document.createElement('p');
+    scale.textContent = `scale: ${Car.scale}`;
+    outputInventory.appendChild(scale);
+   }
+   if (Car.material){
+    const material = document.createElement('p');
+    material.textContent = `material: ${Car.material}`;
+    outputInventory.appendChild(material);
+   }
+   if (Car.modelNumber){
+    const modelNumber = document.createElement('p');
+    modelNumber.textContent = `modelNumber: ${Car.modelNumber}`;
+    outputInventory.appendChild(modelNumber);
+   }
+   if (Car.tire){
+    const tire = document.createElement('p');
+    tire.textContent = `Price: ${Car.tire}`;
+    outputInventory.appendChild(tire);
+   }
+   if (Car.country){
+    const country = document.createElement('p');
+    country.textContent = `country: ${Car.country}`;
+    outputInventory.appendChild(country);
+   }
+   if (Car.year){
+    const year = document.createElement('p');
+    year.textContent = `year: ${Car.year}`;
+    outputInventory.appendChild(year);
+   }
+   if (Car.sku){
+    const sku = document.createElement('p');
+    sku.textContent = `sku: ${Car.sku}`;
+    outputInventory.appendChild(sku);
+   }
+   if (Car.seats){
+    const seats = document.createElement('p');
+    seats.textContent = `seats: ${Car.seats}`;
+    outputInventory.appendChild(seats);
+   }
+   if (Car.trunk){
+    const trunk = document.createElement('p');
+    trunk.textContent = `trunk: ${Car.trunk}`;
+    outputInventory.appendChild(trunk);
+   }
+   if (Car.doors){
+    const doors = document.createElement('p');
+    doors.textContent = `doors: ${Car.doors}`;
+    outputInventory.appendChild(doors);
+   }
+}
+
+
+
