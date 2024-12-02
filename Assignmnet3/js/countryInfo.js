26935c1ada0c6ad091f25f949090cd6355dd22ec
aaliyah033 <79179237+aaliyah033@users.noreply.github.com>
2024-12-02 01:14:04 -0500

    step 4b: TimeZone details

--- Assignmnet3/js/countryInfo.js
+++ Assignmnet3/js/countryInfo.js
@@ -40,6 +40,14 @@ function getCityCountryInfo() {
             const lat = openWeatherResponse.coord.lat; //from JSON format API response example - "coord" lat & lon: 
             const lon = openWeatherResponse.coord.lon; //from JSON format API response example - "coord" lat & lon:
 
+        //STEP 4b: TimeZone details -- using fetch (lesson-11)
+        let googleMapsUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lon}&timestamp&key=${GOOGLE_MAPS_API_KEY}`; // added lat and lon from the openWeatherUrl
+        fetch(googleMapsUrl) 
+        .then(googleMapsResponse => googleMapsResponse.json())//converts to the JSON 
+        .then(googleMapsResponse => {
+            //STEP 4c: ExchangeRate details -- using fetch (lesson-11)
+
+
         
 
         
