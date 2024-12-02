63883d108433fe083ed2fa277cd7d52d72484b07
aaliyah033 <79179237+aaliyah033@users.noreply.github.com>
2024-12-01 23:33:25 -0500

    added step 1: API Keys

---
+++ Assignmnet3/js/countryInfo.js
@@ -0,0 +1,10 @@
+/** API to be used: 
+ * 'eed16b8801cb9ade8b710dbc7a86c828'; // OpenWeather API key
+ * 'AIzaSyBwv9jSYknoJWG2n4nljZN_USXJfjCAjhc'; // Google Maps API key
+ * '1874880fe8e34b4b15276c73'; // ExchangeRate API key
+ */
+
+//STEP 1: API KEY - declared and initialized
+const openWeatherAPIKey = 'eed16b8801cb9ade8b710dbc7a86c828';
+const googleMapsAPIKey = 'AIzaSyBwv9jSYknoJWG2n4nljZN_USXJfjCAjhc';
+const exchangeRateAPIKey = '1874880fe8e34b4b15276c73';
