a2f9007de895df1862c58df3bbfe21bfeeb6d3df
aaliyah033 <79179237+aaliyah033@users.noreply.github.com>
2024-12-01 23:45:48 -0500

    Step 2: targeting the HTML

--- Assignmnet3/js/countryInfo.js
+++ Assignmnet3/js/countryInfo.js
@@ -5,6 +5,14 @@
  */
 
 //STEP 1: API KEY - declared and initialized
+//NOTES: reference from lesson 11
 const openWeatherAPIKey = 'eed16b8801cb9ade8b710dbc7a86c828';
 const googleMapsAPIKey = 'AIzaSyBwv9jSYknoJWG2n4nljZN_USXJfjCAjhc';
 const exchangeRateAPIKey = '1874880fe8e34b4b15276c73';
+
+//STEP 2: target the HTML 
+const cityApi = document.querySelector('#city');
+const countryApi = document.querySelector('#country');
+const searchBtnApi = document.querySelector('#searchBtn');
+const resetBtnApi = document.querySelector('#resetBtn');
+const resultAiOutput = document.querySelector('#resultsDetail');
