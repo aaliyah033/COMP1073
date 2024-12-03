2ceefd02880a6fd4b7cb31c26a13f4bf71a6fe59
aaliyah033 <79179237+aaliyah033@users.noreply.github.com>
2024-12-02 19:51:17 -0500

    made some chnages to make it work

--- Assignmnet3/js/countryInfo.js
+++ Assignmnet3/js/countryInfo.js
@@ -22,6 +22,7 @@ function getCityCountryInfo() {
     //STEP 3a: input validation --- this is to first check if user enters city and country (since i'm doing about countires and cities)
     const cityValidate = cityApi.value.trim();
     const countryValidate = countryApi.value.trim();
+
     if (!cityValidate || !countryValidate ) {
         resultAiOutput.textContent = 'Please you have to enter both COUNTRY & CITY name.'; 
         resultAiOutput.style.visibility = 'visible'; //making the mssh visible
@@ -40,6 +41,9 @@ function getCityCountryInfo() {
         fetch(openWeatherUrl)
         .then(openWeatheResponse => openWeatheResponse.json())//converts to the JSON (the outout as from the document)
         .then(openWeatherDetails => { 
+            if (!openWeatherDetails.coord){
+                throw new Error ('Invalid Response')
+            }
             //since the google maps requre the lat & lon and from information pulled from the openWeather.
             //NOTES: trying to connect three API's together...
             console.log(openWeatherDetails)
@@ -48,14 +52,15 @@ function getCityCountryInfo() {
 
         //STEP 4b: TimeZone details -- using fetch (lesson-11)
         const timestamp = Math.floor(Date.now() / 1000); // Current time in seconds
-        const googleMapsUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lon}&timestamp=${timestamp}&key=${googleMapsAPIKey}`; // added lat and lon from the openWeatherUrl
+        const googleMapsUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lon}&timestamp=${timestamp}&key=${googleMapsAPIKey}`;
+
         return fetch(googleMapsUrl) 
             .then(googleMapsResponse => googleMapsResponse.json())//converts to the JSON 
             .then(googleMapsDetails => {
 
             //STEP 4c: ExchangeRate details -- using fetch (lesson-11)
-        const exchangeRateUrl = `https://v6.exchangerate-api.com/v6/53535e11716700db95019d7d/latest/USD`
-        return fetch(exchangeRateUrl)
+            const exchangeRateUrl = `https://v6.exchangerate-api.com/v6/${exchangeRateAPIKey}/latest/USD`;
+            return fetch(exchangeRateUrl)
             .then(exchangeRateResponse => exchangeRateResponse.json())//converts to the JSON
             .then(exchangeRateDetails => {
                  //STEP 5: Displaying details 
@@ -67,7 +72,7 @@ function getCityCountryInfo() {
                 //STEP 6: Error added for each API 
                  //now for the ERRORS - (lesson-11, nytimes.js)
                 .catch(error => { 
-                    console.error('Error:', error);
+                    console.log('Error:', error);
                     resultAiOutput.textContent = `Error: No currency information.`;
                     resultAiOutput.style.visibility = 'visible';
                     
@@ -75,14 +80,14 @@ function getCityCountryInfo() {
                 });
                     })
                     .catch(error => {
-                        console.error('Error:', error);
+                        console.log('Error:', error);
                         resultAiOutput.textContent = `Error: No timezone information.`
                         resultAiOutput.style.visibility = 'visible';
                        
                         });
                         })
                         .catch(error => {
-                            console.error('Error:', error);
+                            console.log('Error:', error);
                             resultAiOutput.textContent = `Error: No weather information.`
                             resultAiOutput.style.visibility = 'visible';
                             
@@ -94,23 +99,22 @@ function getCityCountryInfo() {
 //let APIUrl = chosenCountry(openWeatherDetails,googleMapsDetails,exchangeRateDetails);
 function chosenCountry (openWeatherDetails, googleMapsDetails, exchangeRateDetails){
     
-    let APIUrl = 
+    return (
     `Information entered:\n`+
     `City: ${openWeatherDetails.name}\n`+
     `Country: ${openWeatherDetails.sys.country}\n\n` +
 
     `Weather Information\n` +
     `Weather Description: ${openWeatherDetails.weather[0].description}\n` +
-    `Temperature: ${openWeatherDetails.main.temp}°C\n` +
-    `Feels Like: ${openWeatherDetails.main.feels_like}°C\n` +
+    `Temperature: ${(openWeatherDetails.main.temp )}°C\n` +
+    `Feels Like: ${(openWeatherDetails.main.feels_like)}°C\n` +
     `Humidity: ${openWeatherDetails.main.humidity}%\n` +
     `Wind Speed: ${openWeatherDetails.wind.speed} m/s\n` +
     `Clouds: ${openWeatherDetails.clouds.all}\n\n` +
 
     `Time Zone Information\n` +
-    `Time Zone: ${googleMapsDetails.timezone}\n` +
-    // `Time Zone ID: ${googleMapsDetails.timezoneId}\n` +
-    `Time Zone Name: ${googleMapsDetails.timezoneName}\n\n` +
+    `Time Zone ID: ${googleMapsDetails.timeZoneId}\n` +  
+    `Time Zone Name: ${googleMapsDetails.timeZoneName}\n\n` +
 
     `Currency Information\n` +
     `Base Currency: ${exchangeRateDetails.base_code}\n` + //from the document "base_code": "USD",
@@ -122,19 +126,17 @@ function chosenCountry (openWeatherDetails, googleMapsDetails, exchangeRateDetai
     `AUD Rate: ${exchangeRateDetails.conversion_rates.AUD}\n` +
     `CAD Rate: ${exchangeRateDetails.conversion_rates.CAD}\n` +
     `TZS Rate: ${exchangeRateDetails.conversion_rates.TZS}\n` +
-    `AED Rate: ${exchangeRateDetails.conversion_rates.AED}\n` +
-
-    //For Testing...
-    console.log(APIUrl);
-    return APIUrl; //to return the items
+    `AED Rate: ${exchangeRateDetails.conversion_rates.AED}\n` 
+    );
+    
 }
 
 //STEP 8: Now for reset button 
 function resetButton(){
     cityApi.value = ''; //this will clear the city input 
     countryApi.value = ''; //this will clear the country input
-    resultAiOutput.value = ''; //this will clear result
-    resultAiOutput.textContent = ''; //this will clear the results provided  
+    resultAiOutput.textContent = ''; //this will clear the results provided 
+    resultAiOutput.style.visibility ='hidden';
 }
 
 //STEP 9: Now for the event listener for the two buttons (reset and search)
@@ -144,9 +146,7 @@ document.getElementById('searchBtn').addEventListener('click',getCityCountryInfo
 
 
                  
-
                
-
 
             
 
